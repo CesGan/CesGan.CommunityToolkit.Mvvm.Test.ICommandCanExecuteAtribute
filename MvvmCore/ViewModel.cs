7b3a4f11d9e2f0f8cf7c597abd77f85ab59ea108
Cesar Pinto <cesar_pinto20@hotmail.com>
2022-05-26 13:37:38 +0100

    added viewmodelbase on a new project added viewmodelbaseinsideproject

--- MvvmCore/ViewModel.cs
+++ MvvmCore/ViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ViewModeBase;
 
 namespace MvvmCore 
 {
     public partial class ViewModel : ViewModelBase
     {
+
         [ICommand(CanExecute = nameof(CanSave))]
         public override void Save()
         {
--- ViewModeBase/ViewModelBase.cs
+++ ViewModeBase/ViewModelBase.cs
@@ -1,6 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace MvvmCore
+namespace ViewModeBase
 {
     public abstract partial class ViewModelBase : ObservableObject
     {
